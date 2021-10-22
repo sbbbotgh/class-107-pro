84a405513de5ca7feee46b04898d09389d438ff0
sbbbotgh <76098746+sbbbotgh@users.noreply.github.com>
2021-10-22 03:21:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia('audio:true');
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/awZ02m2Ty/model.json', modelReady);
+}
+
+function modelReady(){
+   classifier.classify(gotResults);
+}
+function gotResults(error,results){
+    console.log('gotResults');
+}
